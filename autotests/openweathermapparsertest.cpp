e34c7a2aff9ae05e0f80a6c92a788bd2f9ae94ee
David Faure <faure@kde.org>
2016-08-17 22:57:14 +0200

    more unittesting

--- autotests/openweathermapparsertest.cpp
+++ autotests/openweathermapparsertest.cpp
@@ -13,15 +13,16 @@ class OpenWeatherMapParserTest : public QObject
 
 private Q_SLOTS:
     void fileDataProviderShouldProvideData();
+    void parserShouldFetchDatesAndData_data();
     void parserShouldFetchDatesAndData();
 
 private:
-    void fetchData(QByteArray *data);
+    void fetchData(QByteArray *data, const QString &fileName);
 };
 
-void OpenWeatherMapParserTest::fetchData(QByteArray *data)
+void OpenWeatherMapParserTest::fetchData(QByteArray *data, const QString &fileName)
 {
-    DataProvider::Ptr provider(new FileDataProvider(QFINDTESTDATA("openweathermap.json")));
+    DataProvider::Ptr provider(new FileDataProvider(QFINDTESTDATA(fileName)));
     provider->ensureDataAvailable();
     QSignalSpy spyError(provider.data(), &DataProvider::error);
     QSignalSpy spyDataAvailable(provider.data(), &DataProvider::dataAvailable);
@@ -34,19 +35,37 @@ void OpenWeatherMapParserTest::fetchData(QByteArray *data)
 void OpenWeatherMapParserTest::fileDataProviderShouldProvideData()
 {
     QByteArray data;
-    fetchData(&data);
+    fetchData(&data, "openweathermap.json");
+}
+
+void OpenWeatherMapParserTest::parserShouldFetchDatesAndData_data()
+{
+    QTest::addColumn<QString>("fileName");
+    QTest::addColumn<QString>("expectedFirst");
+    QTest::addColumn<QString>("expectedLast");
+
+    QTest::newRow("openweathermap.json") << "openweathermap.json"
+                                         << QStringLiteral("2016-08-11 21:00:00.000 CEST, temp=18, wind=(25, 0, 0), rain=0, icon=http://openweathermap.org/img/w/01n.png")
+                                         << QStringLiteral("2016-08-16 18:00:00.000 CEST, temp=25, wind=(14, 0, 0), rain=0, icon=http://openweathermap.org/img/w/01d.png");
+    QTest::newRow("openweathermap-2.json") << "openweathermap-2.json"
+                                         << QStringLiteral("2016-08-14 21:00:00.000 CEST, temp=13, wind=(5, 0, 0), rain=0, icon=http://openweathermap.org/img/w/01n.png")
+                                         << QStringLiteral("2016-08-19 18:00:00.000 CEST, temp=22, wind=(28, 0, 0), rain=0, icon=http://openweathermap.org/img/w/01d.png");
 }
 
 void OpenWeatherMapParserTest::parserShouldFetchDatesAndData()
 {
+    QFETCH(QString, fileName);
+    QFETCH(QString, expectedFirst);
+    QFETCH(QString, expectedLast);
+
     QByteArray data;
-    fetchData(&data);
+    fetchData(&data, fileName);
 
     OpenWeatherMapParser parser;
     QVector<WeatherDataEntry> wdlist = parser.parse(data);
     QCOMPARE(wdlist.count(), 40);
-    QCOMPARE(wdlist.first().toString(), QStringLiteral("2016-08-11 21:00:00.000 CEST, temp=18, wind=(25, 0, 0), rain=0, icon=http://openweathermap.org/img/w/01n.png"));
-    QCOMPARE(wdlist.last().toString(), QStringLiteral("2016-08-16 18:00:00.000 CEST, temp=25, wind=(14, 0, 0), rain=0, icon=http://openweathermap.org/img/w/01d.png"));
+    QCOMPARE(wdlist.first().toString(), expectedFirst);
+    QCOMPARE(wdlist.last().toString(), expectedLast);
 }
 
 QTEST_MAIN(OpenWeatherMapParserTest)
