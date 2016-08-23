23bcccca116b5a5b3c543b8596001f545784a6d4
David Faure <faure@kde.org>
2016-08-24 00:03:10 +0200

    Add cmake invocation for building for the RPI.
    Requires a hand-written cmake toolchain file.

---
+++ mycmake-rpi.sh
@@ -0,0 +1,1 @@
+cmake $* -G'Unix Makefiles' -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_TOOLCHAIN_FILE=/home/dfaure/src/perso/raspberrypi/pi-toolchain.cmake -DCMAKE_PREFIX_PATH=/home/dfaure/src/perso/raspberrypi/qt5pi
