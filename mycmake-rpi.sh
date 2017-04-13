c827acdfae43864b92472589bfc44fc69e297ee4
David Faure <faure@kde.org>
2017-04-13 18:33:46 +0200

    moved to own repo

--- mycmake-rpi.sh
+++
@@ -1,1 +0,0 @@
-cmake $* -G'Unix Makefiles' -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_TOOLCHAIN_FILE=/home/dfaure/src/perso/raspberrypi/pi-toolchain.cmake -DCMAKE_PREFIX_PATH=/d/kdab/src/rpi/tools/qt5pi
